aba176a90b966de56bde56a153df8ff4e75bf259
narendra-tig <narendra.nagarajan@open360.com>
2026-07-30 14:40:11 +1000

    feat: add Prisma services for accounts, core, documents, freight, and tracking

---
+++ src/prisma/tracking-prisma.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../models/tracking/client';
+
+@Injectable()
+export class TrackingPrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const isPrismaUrl = /^prisma(?:\+postgres)?:\/\//.test(connectionString);
+
+    super(
+      isPrismaUrl
+        ? { accelerateUrl: connectionString }
+        : { adapter: new PrismaPg({ connectionString }) },
+    );
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
